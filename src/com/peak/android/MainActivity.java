b3ff468f9974409f6f6fca69b096e2221eb422c7
GustavoFernandes <gusgusgus222@gmail.com>
2013-08-04 02:47:30 -0400

    .

--- src/com/peak/android/MainActivity.java
+++ src/com/peak/android/MainActivity.java
@@ -2,19 +2,61 @@ package com.peak.android;
 
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.EditText;
-import android.widget.TextView;
-import org.json.JSONObject;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 
 public class MainActivity extends Activity {
+
+    SlidingMenu menu;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        //setBehindContentView(R.layout.main);
+
+        /*
+        SlidingMenu menu = new SlidingMenu(this);
+        menu.setMode
+        menu.setMode(SlidingMenu.LEFT);
+        menu.setBehindScrollScale(0.0f);
+        menu.setFadeDegree(0.0f);
+        menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+        menu.setMenu(R.layout.menu);
+        */
+
+        menu = new SlidingMenu(this);
+        menu.setMode(SlidingMenu.LEFT); // Menu only appears from left.
+        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN); // Can display menu from dragging anywhere.
+        menu.setTouchModeBehind(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        menu.setBehindOffset(300);
+        menu.setFadeDegree(0.3f);
+        menu.setShadowWidth(5);
+        //menu.setOnClickListener(this);
+        menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+        menu.setMenu(R.layout.menu);
+
+        Button btn = (Button) findViewById(R.id.menuButton1);
+        btn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "sliding menu button click", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        //menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        //menu.setShadowWidthRes(R.dimen.shadow_width);
+        //menu.setShadowDrawable(R.drawable.shadow);
+        //menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+        //menu.setFadeDegree(0.35f);
+        //menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+        //menu.setMenu(R.layout.login);
+
+        /*
 
         Log.d("Peak", "MainActivity onCreate()");
 
@@ -30,5 +72,11 @@ public class MainActivity extends Activity {
         catch(Exception e) {
             Log.e("Peak", e.toString());
         }
+
+        */
+    }
+
+    public void toggle(View v) {
+        menu.toggle(true);
     }
 }
